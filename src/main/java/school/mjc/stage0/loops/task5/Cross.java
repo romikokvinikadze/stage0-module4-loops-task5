c32f9822bbf63e1e8cb6933bdd71412cb3b0aae6
romikokvinikadze <r.kvinikadze11@gmail.com>
2022-07-01 14:45:52 +0400

    nested loops

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
+        int k = sideLength / 2;
 
+        for (int i=0; i<sideLength; i++){
+            for (int j=0; j<sideLength; j++){
+                if (i == k || j == k){
+                    System.out.print('8');
+                }else System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -3,5 +3,23 @@ package school.mjc.stage0.loops.task5;
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
 
+        for (int i=0; i<height; i++){
+            for (int j=0; j<height; j++){
+                int k = getK(height, i);
+                if (k <= j && j <= height - k - 1){
+                    System.out.print("8");
+                }else System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int getK(int height, int current) {
+        if (current < height / 2) return current;
+        return height - 1 - current;
+    }
+
+    public static void main(String[] args) {
+        new Hourglass().printHourglassOfGivenSize(4);
     }
 }
